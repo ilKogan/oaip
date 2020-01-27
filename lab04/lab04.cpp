0744543ca74cbc4b7a32fd6b0b4cccdf608c5f5b
ilKogan <vinsent.in7aider@gmail.com>
2020-01-27 10:53:13 +0300

    Modify old task lab4 and add new

--- lab04/lab04.cpp
+++ lab04/lab04.cpp
@@ -1,9 +1,42 @@
 ﻿#include <iostream>
 #include <iomanip>
+#include <windows.h>
+
 
 
 using namespace std;
 
+struct vector2
+{
+	int x;
+	int y;
+};
+
+vector2 ConsoleInfo()
+{
+
+	vector2 screen;
+
+	HANDLE hWndConsole;
+	if (hWndConsole = GetStdHandle(-12))
+	{
+		CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+		if (GetConsoleScreenBufferInfo(hWndConsole, &consoleInfo))
+		{
+			 screen.x = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+			 screen.y = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+			
+		}
+		else
+			printf("Error: %d\n", GetLastError());
+	}
+	else
+		printf("Error: %d\n", GetLastError());
+	
+
+	return screen;
+
+}
 
 void Exit(void(*f)())
 	{
@@ -31,38 +64,33 @@ void Exit(void(*f)())
 	}
 void SelectAndSizeOfType()
 	{
-		string input;
+		int input;
 
 		cout << endl;
 		cin >> input;
 
-		if (input == "INT")
+		switch (input)
 		{
-			cout << sizeof(int) << " байт";
+		case 1:
+			cout << sizeof(int) << " byte";
 			cout << endl;
+			break;
+		case 2:
+			cout << sizeof(float) << " byte";
+			cout << endl;
+			break;
+		case 3:
+			cout << sizeof(double) << " byte";
+			cout << endl;
+			break;
+		case 4:
+			cout << sizeof(char) << " byte";
+			cout << endl;
+			break;
+		default:
+			break;
 		}
-		else
-			if (input == "FLOAT")
-			{
-				cout << sizeof(float) << " байт";
-				cout << endl;
-			}
-			else
-				if (input == "DOUBLE")
-				{
-					cout << sizeof(double) << " байт";
-					cout << endl;
-				}
-				else
-					if (input == "CHAR")
-					{
-						cout << sizeof(char) << " байт";
-						cout << endl;
-					}
-					else
-					{
-						cout << endl << "Ошибка : Введено некоректное название типа" << endl;
-					}
+
 	}
 void DrawTriangle()
 	{
@@ -75,6 +103,7 @@ void DrawTriangle()
 		cin >> ch;
 		cout << "Enter size\n";
 		cin >> empty;
+		cout <<endl;
 	
 		for (int s = empty - 1; s > 0; s--)
 		{
@@ -88,49 +117,245 @@ void DrawTriangle()
 			empty -= 1;
 		}   
     }
+void CalculatePriceMilk()
+	{
+		int nowYear;
+		float nowPrice;
+		float percent ;
+		int years;
+
+		float onePercentPrice;
+		float differencePrice;
+
+		int firstYear;
+		float firstPrice;
+		float secondprice;
+
+
+		cout << endl;
+		cout << "What is the year now?\n";
+		cin >> nowYear;
+		cout << "Price \n";
+		cin >> nowPrice;
+		cout << "Percent % \n";
+		cin >> percent;
+		cout << "Years \n";
+		cin >> years;
+
+		firstPrice = nowPrice;
+		firstYear = nowYear;
+
+		for(years; years > 0 ;years--)
+		{
+			onePercentPrice = nowPrice / 100;
+			differencePrice = onePercentPrice * percent;
+
+			nowYear++;
+			cout<<"Year: "<<nowYear<<endl;
+
+			nowPrice += differencePrice;
+			cout<<"Winter difference Price: "<< differencePrice<<endl;
+			cout<<"Winter nowPrice :"<< nowPrice<<endl;
+
+			onePercentPrice = nowPrice / 100;
+			differencePrice = onePercentPrice * percent;
+
+			nowPrice -= differencePrice;
+			cout<<"Summer difference Price: "<< differencePrice << endl;
+			cout<<"Summer Price: "<< nowPrice << endl << endl;
+			
+
+		}
+
+		cout<<"Year: "<<firstYear<<" Price: "<< firstPrice << endl;
+		cout<<"Year: "<<nowYear<<" Price: "<< nowPrice << endl;
+		cout<<"Difference : -"<< firstPrice - nowPrice << endl << endl;
+	}
+void DrawTriangleTwo()
+{
+
+	char ch,
+		space = ' ';
+	int pattern = -1;
+	int empty;
+	int ident;
+	
+	cout << "Enter char\n";
+	cin >> ch;
+	cout << endl;
+
+	ident = floor(30 * ConsoleInfo().x / 100);
+	empty = floor(75 * ConsoleInfo().y / 100);
+
+
+	for (int s = empty - 1; s > 0; s--)
+	{
+
+		pattern += 2;
+
+		cout << setw(ident) << setfill(space) << space;
+		cout << setw(empty) << setfill(space) << space;
+		cout << setw(pattern) << setfill(ch) << ch;
+		cout << setw(empty) << setfill(space) << space << endl;
+
+		empty -= 1;
+	}
+
+	cout << "Console Window\n Weight:" << ConsoleInfo().x << endl;
+	cout << "Height:" << ConsoleInfo().y << endl;
+}
+
+
+void DistanceTwoPoint()
+	{
+		vector2 m1,m2;
+		float distance;
+
+		cout<<"\nEnter the value of x and y point m1\n";
+		cin>>m1.x>>m1.y;
+		cout<<"Enter the value of x and y point m2\n";
+		cin>>m2.x>>m2.y;
+
+		distance = sqrt(pow((m2.x - m1.x), 2)+ pow((m2.y - m1.y),2));
+
+		cout<<"Distance :"<<distance;
+	}
+void DrawStar()
+	{
+		const int height = 23;
+		const int weight = 48;
+		
+		char pattern;
+		char empty = ' ';
+		bool matrix[height][weight] = {
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0},
+			{0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+			};
+
+		cout<< "Enter char\n";
+		cin >> pattern;
+
+		int ident = floor(30 * ConsoleInfo().x / 100);
+
+		for(int i = 0;i < height;i++)
+		{
+			cout << setw(ident) << setfill(empty) << empty;
+			for(int j = 0;j < weight; j++)
+			{
+				if(matrix[i][j]==false)
+				{
+					cout<<empty;
+				}else{
+					cout<<pattern;
+				}
+			}
+			cout<<endl;
+		}
+		
+
+
+	}
+
+
+
+
+
 
 int main()
 {
 	void(*SelPtr)() = &SelectAndSizeOfType;
 	void(*DrawTrianglePtr)() = &DrawTriangle;
+	void(*CalculatePriceMilkPtr)() = &CalculatePriceMilk;
+	void(*DistanceTwoPointPtr)() = &DistanceTwoPoint;
+	void(*DrawStarPtr)() = &DrawStar;
+	void(*DrawTriangleTwoPtr)() = &DrawTriangleTwo;
 
-#pragma region
+// #pragma region--------------TASK 1----------------
 
-	printf("Task 1");
+// 	printf("Task 1\n");
 
-	printf("This program shows the size of an object in byte.");
-	printf("To start program, select the type\n\nINT\nFLOAT\nDOUBLE\nCHAR\n");
-
-
-
-	Exit(SelPtr);
-#pragma endregion --------------TASK 1----------------
-#pragma region
-
-	printf("\nTask 2\n");
-	printf("\nThis program draws a triangle\n");
-
-	Exit(DrawTriangle);
-#pragma endregion --------------TASK 2----------------
-#pragma region
-	printf("Task 3");
-	printf("Sinitsa \t\t\t Faculty: Huevpinanie \nElisey \t\t\t Number group: Hmm \nDmitrievicc\bh\n");
-	printf("text with one letter missing \r \n\n");
-
-	system("pause");
-#pragma endregion --------------TASK 3----------------
-#pragma region
-	printf("Task 4");
+// 	printf("This program shows the size of an object in byte.");
+// 	printf("To start program, select the type\n\n1.INT\n2.FLOAT\n3.DOUBLE\n4.CHAR\n");
 
 
-	char name[60];
+
+// 	Exit(SelPtr);
+// #pragma endregion This program shows the size of an object in byte
+// #pragma region--------------TASK 2----------------
+
+// 	printf("\nTask 2\n");
+// 	printf("\nThis program draws a triangle\n");
+
+// 	Exit(DrawTriangle);
+// #pragma endregion This program draws a triangle
+// #pragma region--------------TASK 3----------------
+// 	printf("Task 3\n");
+// 	printf("Sinitsa \t\t\t Faculty: Huevpinanie \nElisey \t\t\t Number group: Hmm \nDmitrievicc\bh\n");
+// 	printf("text with one letter missing \r \n\n");
+
+// 	system("pause");
+// #pragma endregion This program print my name and other
+// #pragma region--------------TASK 4----------------
+// 	printf("Task 4\n");
+
+// 	SetConsoleOutputCP(1251);
+// 	SetConsoleCP(1251);
+
+// 	char name[60];
 
 
-	puts("What is your name?");
-	gets_s(name);
-	printf("Hello, %s ", &name);
-#pragma endregion --------------TASK 4----------------
+// 	puts("What is your name?\n");
+// 	gets_s(name);
+// 	printf("Hello, %s ", &name);
+// #pragma endregion This program simulation dialog
+#pragma region--------------TASK 5.1----------------
+	printf("\nTask 5.1\n");
+	printf("\nThis program draws a triangle in center of console window\n");
+	
 
+	Exit(DrawTriangleTwo);
+#pragma endregion This program draws a triangle in center of console window
+// #pragma region--------------TASK 5.2----------------
+// 	printf("\nTask 5.2\n");
+// 	printf("\nFarmer at the beginning of winter increases\n");
+// 	printf("the nowPrice of milk at p procent and in the summer reduces by\n");
+// 	printf("the same percentage. whether the nowPrice change on milk\n");
+// 	printf("and, if so, in what direction and by how much, after n years?\n");
 
+// 	Exit(CalculatePriceMilk);
+// #pragma endregion This program solves the problem about a farmer
+#pragma region--------------TASK 6.1----------------
+	printf("\nTask 6.1\n");
+	printf("\nDraw Star\n");
+	Exit(DrawStar);
+#pragma This program draw Star
+#pragma region--------------TASK 6.2----------------
+	printf("\nTask 6.2\n");
+	printf("\nFinding the distance between two points\n");
+	
+
+	Exit(DistanceTwoPoint);
+#pragma Finding the distance between two points
 
 }
