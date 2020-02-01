fdec841cb43ffc7766ac4cd18d9677d71b9be992
ilKogan <vinsent.in7aider@gmail.com>
2020-02-01 15:53:32 +0300

    formatting code

--- lab06/Lab06.cpp
+++ lab06/Lab06.cpp
@@ -9,146 +9,146 @@ using namespace std;
 
 void Task1()
 {
-   float z,y,x;
-   
-   
-   for (int n = 0; n < 4; n++)
-   {
+	float z, y, x;
 
-       printf("\nWrite x \n");
 
-       scanf_s("%f", &x);
-       z = 2 * pow(x, 2);
-       y = z + pow(x, sd);
+	for (int n = 0; n < 4; n++)
+	{
 
-       printf("\nx = %5.2f\t", x);
-       printf("\ny = %5.2f\t", y);
+		printf("\nWrite x \n");
 
-   }
+		scanf_s("%f", &x);
+		z = 2 * pow(x, 2);
+		y = z + pow(x, sd);
+
+		printf("\nx = %5.2f\t", x);
+		printf("\ny = %5.2f\t", y);
+
+	}
 
 }
 void Task2()
 {
-   float z,y,x = 3;
-   while(x < 4.1)
-   {
-      z = 2 * pow(x, 2);
-      y = z + pow(x, (float)1 / 3);
-      cout <<"x="<< x <<"\t\n";
-      cout <<"y="<< y <<"\t\n";
-      x = x + 0.1;
-   }
+	float z, y, x = 3;
+	while (x < 4.1)
+	{
+		z = 2 * pow(x, 2);
+		y = z + pow(x, (float)1 / 3);
+		cout << "x=" << x << "\t\n";
+		cout << "y=" << y << "\t\n";
+		x = x + 0.1;
+	}
 }
 void Task3()
 {
-    float z,y,x = 3;
+	float z, y, x = 3;
 
-    do
-    {
-      z = 2 * pow(x, 2);
-      y = z + pow(x, (float)1 / 3);
-      cout <<"x="<< x <<"\t\n";
-      cout <<"y="<< y <<"\t\n";
-      x = x + 0.1;
-    } while (x < 4.1);
+	do
+	{
+		z = 2 * pow(x, 2);
+		y = z + pow(x, (float)1 / 3);
+		cout << "x=" << x << "\t\n";
+		cout << "y=" << y << "\t\n";
+		x = x + 0.1;
+	} while (x < 4.1);
 }
 void Task4()
 {
-   int n, i, j;
-   printf("Enter n: \n");
+	int n, i, j;
+	printf("Enter n: \n");
 
-   scanf_s("%d", &n);
+	scanf_s("%d", &n);
 
-   for(i = 1; i <= n; i++)
-   {
-       for (j = 1; j <= n; j++)
-       {
-           printf("%5d", i * j);
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			printf("%5d", i * j);
 
-           if (i == 1)
-           {
-               cout << endl;
-               cout << setw(n * 5) << setfill(tir) << tir << endl;
-           }
+			if (i == 1)
+			{
+				cout << endl;
+				cout << setw(n * 5) << setfill(tir) << tir << endl;
+			}
 
-           printf("\n");
-       }
+			printf("\n");
+		}
 
-   }
+	}
 
 }
 void Task5()
 {
-    float y, z, a, b, n, i;
-    int choice;
-    a = 2 * pow(10, -3);
-    b = 8,5;
+	float y, z, a, b, n, i;
+	int choice;
+	a = 2 * pow(10, -3);
+	b = 8, 5;
 
-    cout<<"-----Menu-----\n";
-    cout<<"choice calculation variant\n";
-    cout << "1. for()\n2. while\n 3.for(){for()}\n";
-    cin>> choice;
+	cout << "-----Menu-----\n";
+	cout << "choice calculation variant\n";
+	cout << "1. for()\n2. while\n 3.for(){for()}\n";
+	cin >> choice;
 
-    switch (choice)
-    {
-    case 1: 
-        for (n = 0; n < 2; n++)
-        {
-    
-            cout << "\nWrite i \n";
+	switch (choice)
+	{
+	case 1:
+		for (n = 0; n < 2; n++)
+		{
 
-            scanf_s("%f", &i);
+			cout << "\nWrite i \n";
 
-            y = sqrt((i * b) - (pow(b, 2) * a));
-            z = y * tan(n / 4) - exp(1 + b);
+			scanf_s("%f", &i);
 
-            cout << "\ny = " << y << endl;
-            cout << "\nz = " << z << endl;
+			y = sqrt((i * b) - (pow(b, 2) * a));
+			z = y * tan(n / 4) - exp(1 + b);
 
-        }
-        break;
-    case 2:
-        do
-        {
-            cout << "\nWrite i \n";
-            scanf_s("%f", &i);
-            cout << "\nWrite n \n";
-            scanf_s("%f", &n);
+			cout << "\ny = " << y << endl;
+			cout << "\nz = " << z << endl;
+
+		}
+		break;
+	case 2:
+		do
+		{
+			cout << "\nWrite i \n";
+			scanf_s("%f", &i);
+			cout << "\nWrite n \n";
+			scanf_s("%f", &n);
 
 
-            y = sqrt((i * b) - (pow(b, 2) * a));
-            z = y * tan(n / 4) - exp(1 + b);
+			y = sqrt((i * b) - (pow(b, 2) * a));
+			z = y * tan(n / 4) - exp(1 + b);
 
-            cout << "\ny = " << y << endl;
-            cout << "\nz = " << z << endl;
-            i += 0.5;
-        } while (i < 3.0);
-        break;
-    case 3:
-        for (i = 1; i < 3; i += 0,5)
-        {
-            for (int n = 2; n < 3; n += 0.5)
-            {
-                y = sqrt((i * b) - (pow(b, 2) * a));
-                z = y * tan(n / 4) - exp(1 + b);
-    
-                cout << "\ny = " << y;
-                cout << " z = " << z << endl;
-            }
+			cout << "\ny = " << y << endl;
+			cout << "\nz = " << z << endl;
+			i += 0.5;
+		} while (i < 3.0);
+		break;
+	case 3:
+		for (i = 1; i < 3; i += 0, 5)
+		{
+			for (int n = 2; n < 3; n += 0.5)
+			{
+				y = sqrt((i * b) - (pow(b, 2) * a));
+				z = y * tan(n / 4) - exp(1 + b);
 
-        }
-        break;
-    default:
-        break;
-    }
+				cout << "\ny = " << y;
+				cout << " z = " << z << endl;
+			}
 
-   
+		}
+		break;
+	default:
+		break;
+	}
+
+
 
 }
 
 int main()
 {
-    Task5();
+	Task5();
 }
 
 
