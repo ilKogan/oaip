13585200d233f3f5db93c517b8007b11a7652d04
ilKogan <vinsent.in7aider@gmail.com>
2020-02-07 15:53:53 +0300

    Add folder lab4

--- Task3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void Task3() {
-	printf("Task 3\n");
-	printf("Sinitsa \t\t\t Faculty: Huevpinanie \nElisey \t\t\t Number group: Hmm \nDmitrievicc\bh\n");
-	printf("text with one letter missing \r \n\n");
-
-	system("pause");
-}
