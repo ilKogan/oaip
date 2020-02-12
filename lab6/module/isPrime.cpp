456ec870f507180a02ef770624e7cfe6077f8bc5
ilKogan <vinsent.in7aider@gmail.com>
2020-02-12 12:04:18 +0300

    func IsSuperPrime modify

--- lab6/module/isPrime.cpp
+++ lab6/module/isPrime.cpp
@@ -28,6 +28,12 @@ bool isPrime(int number, int step = 2) {
 //	}
 //	return convertNumberToArray(number, state);
 //}
+std::string revers(std::string const& s) 
+{
+	std::string rev(s.rbegin(), s.rend());
+	return rev;
+}
+
 void convertNumberToArray(int number, int* state, int& size) {
 	int digit = number % RADIX;
 	number /= RADIX;
@@ -42,19 +48,37 @@ void convertNumberToArray(int number, int* state, int& size) {
 	return convertNumberToArray(number,state, size);
 }
 
+int convertArrayToNumber(int count,int state[],int size,std::string fullnumber)
+{
+	fullnumber = (fullnumber + std::to_string(state[count++]));
+	if (size == count) 
+	{	
+		fullnumber = revers(fullnumber);
+		return std::stoi(fullnumber);
+	}
+	return convertArrayToNumber(count,state,size,fullnumber);
+}
+
+
 bool isSuperPrime(int number) {
 		
 	int size = 0;
 	int* state = new int;
+	int result;
+
 	convertNumberToArray(number, state, size);
 	
-
 	do {
 		//TODO:: Array of ints to number
 		//TODO:: CHECK IS PRIME
-		//
-		std::cout << state[0] << state[1] <<std::endl;
-		//isPrime(numbera)
+		/*std::cout << state[0] << state[1] <<state[3]<<std::endl*/
+		std::string fullnumber = "";
+
+		result = convertArrayToNumber(0,state,size,fullnumber);
+		if (isPrime(result)) {
+			std::cout << result << std::endl;
+		}
+
 	} while (std::next_permutation(state, state + size));
 
 	return false;
