85b4ae538748bd045c0dfb4261280c7c879c2cdb
ilKogan <vinsent.in7aider@gmail.com>
2020-02-28 18:02:05 +0300

    Task 2.1 , 2.2 complete

--- lab10/module/Task2_1.cpp
+++ lab10/module/Task2_1.cpp
@@ -1,85 +1,61 @@
 #include <iostream> 
 #include <new>
 
+//1. Все четные элементы целочисленного массива K(n) поместить в массив L(n), а нечетные – в массив М(n).Подсчитать количество тех и других.
+//2. Задан массив из k чисел.Сдвинуть элементы массива циклически на n позиций вправо.
 
-//bool checkEvenAndOdd(int i) {
-//		return i % 2 == 0 ? true : false;	
-//}
-//
-//int getSizeArray(int* array[]) {
-//	return sizeof(*array) / sizeof(*array[0]);
-//}
-
-//int getNumberToNumbers(int* numbers[]) {
-//	int size = getSizeArray(*numbers[]);
-//	int count;
-//	if (size != 0) {
-//		for (int i = 0; i <= size - 1; i++) {
-//			if (*numbers[i] != 0) {
-//				count++;
-//			}
-//			else {
-//				return count;
-//			}
-//
-//		}
-//	}
-//}
-
-int fillNumbers(int *(&array)) {
+int fillNumbers(int* (&array)) {
 	int size = 0;
 	std::cin >> size;
 	array = new int[size];
-	 
-		for (int i = 0; i <= size - 1; i++) {
-			array[i] = i;
-		}
-
-		return size;
-}
-
-//void cleanArray(int* numbers[]) {
-//	int size = getSizeArray(numbers);
-//
-//	if (size != 0) {
-//		for (int i = 0; i <= size - 1; i++) {
-//			*numbers[i] = 0;
-//		}
-//	}
-//}
-//
-//void copyEvenAndOddElements(int *numbers[], int *evenNumbers[], int* oddNumbers[]) {
-//	int size = getSizeArray(evenNumbers);
-//
-//	if (size != 0) {
-//		for (int i = 0; i <= size - 1; i++) {
-//			if (checkEvenAndOdd(i)) 
-//			{
-//			 *evenNumbers[i] = *numbers[i];
-//			}
-//			else {
-//				*oddNumbers[i] = *numbers[i];
-//			}
-//		}
-//	}
-//}
-
-
-
-
-
-
-void Task2_1() {
-
-	int *numbers = NULL;
-	int num = 0;
-	//numbers = &num;
-	int size = fillNumbers(numbers);
 
 	for (int i = 0; i <= size - 1; i++) {
-		std::cout << numbers[i] << std::endl;
+		array[i] = i;
+	}
+
+	return size;
+}
+
+void copyEvenAndOddElements(int originSize, int* origin, int* (&even), int* (&odd)) {
+
+	even = new int[originSize];
+	odd = new int[originSize];
+
+	for (int i = 0; i < originSize; i++) {
+
+		even[i] = origin[i] + origin[i];
+		odd[i] = origin[i] + (origin[i] + 1);
 	}
 
 
+
+}
+
+void rightOffsetEllements(int size, int* arr) {
+
+	for (int i = 0; i <= size; i++) {
+
+		if (arr[i] == size-1) {
+			arr[i] = arr[i] + 1;
+		}
+		else {
+			arr[i] = arr[i + 1];
+		}
+		  
+	}
+}
+
+void Task2_1() {
+
+	int* numbers = NULL;
+	int* even = NULL;
+	int* odd = NULL;
+
+
+	int sizeNum = fillNumbers(numbers);
+
+	copyEvenAndOddElements(sizeNum, numbers, even, odd);
+
+	rightOffsetEllements(sizeNum,numbers);
 
 }
