6e3e486a955b6d51988cea3d10e66f7fa27e119c
ilKogan <vinsent.in7aider@gmail.com>
2020-03-10 13:21:04 +0300

    Task 4 complete

---
+++ lab11/module/Task3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+//TODO:FIX THIS!!ebannye bits...
+
+using namespace std;
+void Task3() {
+
+	//char tmp[33];
+	//
+	//int numberA = 243;
+	//int positionA = 2;
+	//int numEditBitsA = 2;
+
+	//int numberB = 14;
+	//int positionB = 2;
+	//int numEditBitsB = 2;
+
+	//cout << "Write numA : "; cin >> numberA;
+	//cout << endl;
+	//cout << "Write posA :";cin >> positionA;
+	//cout << endl;
+	//cout << "Write areA : ";cin >> numEditBitsA;
+	//cout << endl;
+
+	////cout << "Write numB : "; cin >> numberB;
+	////cout << endl;
+	////cout << "Write posB :"; cin >> positionB;
+	////cout << endl;
+	////cout << "Write areB : "; cin >> numEditBitsB;
+	////cout << endl;
+
+	//_itoa_s(numberA, tmp, 2); cout << "A num =       " << tmp << endl;
+	//_itoa_s(numberB, tmp, 2); cout << "B num =           " << tmp << endl;
+
+
+	//int posA = positionA + numEditBitsA;
+	//int posB = positionB + numEditBitsB;
+
+	//for (int a = positionA, b = positionB; a < posA || b < posB; a++, b++) {
+	//	/*numberA ^= (1 << a);*/
+	//	/*numberA ^= ((numberB = (1 << b)) << a);*/
+	//	/*numberA = (numberA & 1) | (numberB & 1) << a;*/
+	//	int maskb = 0;
+
+	//	maskb ^= (numberB << b);
+
+	//	numberA ^= (1 << a);
+
+	//	_itoa_s(maskb, tmp, 2);
+	//	cout << "maskb =      " << tmp << endl;
+
+	//	_itoa_s(numberA, tmp, 2);
+	//	cout << "iteration " << a << " = " << tmp << endl;
+	//}
+	//_itoa_s(numberA, tmp, 2);
+	//cout << "result =      " << tmp << endl;
+}
+
+
+
